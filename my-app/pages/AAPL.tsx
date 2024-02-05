37c868f4038958e861619962a23b6dfe43260171
Steins;ATLA <gauraangpdesai@gmail.com>
2024-02-05 22:29:05 +0530

    API values read from backend

--- my-app/pages/AAPL.tsx
+++ my-app/pages/AAPL.tsx
@@ -1,24 +1,19 @@
 import React from "react"
 import axios from "axios"
 
-// type data = {
-//     Date: string[]
-//     Predicted_Price: number
-//     RMSE: number
-// }
 type resp = {
-    data: { res: string }
+    data: { Date: string[]; Predicted_Price: number[]; RMSE: number }
 }
 
 export default function Home({ data }: resp): React.ReactNode {
-    return <h1>{data.res}</h1>
+    return <h1>{data.RMSE}</h1>
     //
 }
 
 export async function getStaticProps() {
     const response = await axios.get("http://127.0.0.1:8000/")
     const data = response.data
-    console.log(data.res)
+    console.log(data)
     return {
         props: {
             data,
