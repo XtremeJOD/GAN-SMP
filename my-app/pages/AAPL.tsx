7122c21187f47b43a55fa7e85dd0af9d2175c818
Steins;ATLA <gauraangpdesai@gmail.com>
2024-02-06 22:27:03 +0530

    basic chart render

--- my-app/pages/AAPL.tsx
+++ my-app/pages/AAPL.tsx
@@ -1,12 +1,35 @@
 import React from "react"
 import axios from "axios"
+import Chart from "chart.js/auto"
 
 type resp = {
     data: { Date: string[]; Predicted_Price: number[]; RMSE: number }
 }
 
 export default function Home({ data }: resp): React.ReactNode {
-    return <h1>{data.RMSE}</h1>
+    const xValues = data.Date
+    const yValues = data.Predicted_Price
+    new Chart("AAPLchart", {
+        type: "line",
+        data: {
+            labels: xValues,
+            datasets: [
+                {
+                    fill: false,
+                    backgroundColor: "rgba(0,0,255,1.0)",
+                    borderColor: "rgba(0,0,255,0.1)",
+                    data: yValues,
+                },
+            ],
+        },
+        // options: {
+        //   legend: {display: true},
+        //   scales: {
+        //     yAxes: [{ticks: {min: 6, max:16}}],
+        //   }
+        // }
+    })
+    return <canvas id="AAPLchart">{data.RMSE}</canvas>
     //
 }
 
